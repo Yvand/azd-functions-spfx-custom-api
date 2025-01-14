7bc4b1ab59a57ec321216499c5c703a0355201db
Yvand <yvandev@outlook.fr>
2025-01-14 14:35:18 +0100

    use webpart properties

--- spfx-custom-api/src/common.ts
+++ spfx-custom-api/src/common.ts
@@ -1,9 +1,3 @@
-export const CommonConfig = {
-    ClientAppId: "77dd86d9-ea20-4816-89d7-4cc098e685ed",
-    FunctionAppHost: "func-api-azzjijutvy2vc.azurewebsites.net",
-    FunctionAppCode: "Yjnsljk44wahtUzwQkOkco405m87lKYXVQ7o8tO9Ok9_AzFu1anjDw==",
-}
-
 export function formatError(error: unknown): string {
     let errorMessage = "";
     if (error instanceof Error) {
--- spfx-custom-api/src/webparts/customApiWebPart/CustomApiWebPartWebPart.ts
+++ spfx-custom-api/src/webparts/customApiWebPart/CustomApiWebPartWebPart.ts
@@ -1,19 +1,33 @@
 import { Version } from '@microsoft/sp-core-library';
 import { AadHttpClient, HttpClientResponse } from '@microsoft/sp-http';
 import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
-import { CommonConfig, formatError } from '../../common';
+import { IPropertyPaneConfiguration, PropertyPaneTextField }from '@microsoft/sp-property-pane'
+import { formatError } from '../../common';
 import styles from './CustomApiWebPartWebPart.module.scss';
 
 export interface ICustomApiWebPartWebPartProps {
+  clientAppId: string;
+  functionAppHost: string;
+  functionAppCode: string;
 }
 
 export default class CustomApiWebPartWebPart extends BaseClientSideWebPart<ICustomApiWebPartWebPartProps> {
   public async render(): Promise<void> {
     let output: string = "Webpart is loaded";
+    const clientAppId = this.properties.clientAppId;
+    const functionAppHost = this.properties.functionAppHost;
+    const functionAppCode = this.properties.functionAppCode;
+    
+    if (!clientAppId || !functionAppHost || !functionAppCode) {
+      output += "<br>Configuration is missing, edit the webpart to provide the missing values";
+      this.domElement.innerHTML = `<div class="${styles.customApiWebPart}">${output}</div>`;
+      return;
+    }
+    
     try {
-      output += `<br>Getting an access token for the resource '${CommonConfig.ClientAppId}'`;
-      const client: AadHttpClient = await this.context.aadHttpClientFactory.getClient(CommonConfig.ClientAppId);
-      const functionUrl = `https://${CommonConfig.FunctionAppHost}/api/getData?code=${CommonConfig.FunctionAppCode}`;
+      output += `<br>Getting an access token for the resource '${clientAppId}'`;
+      const client: AadHttpClient = await this.context.aadHttpClientFactory.getClient(clientAppId);
+      const functionUrl = `https://${functionAppHost}/api/getData?code=${functionAppCode}`;
       output += `<br>Access token received<br>Connecting to the function app '${functionUrl}'`;
       const response: HttpClientResponse = await client.get(functionUrl, AadHttpClient.configurations.v1);
       output += `<br>Data received:<br>`;
@@ -36,5 +50,44 @@ export default class CustomApiWebPartWebPart extends BaseClientSideWebPart<ICust
 
   protected get dataVersion(): Version {
     return Version.parse('1.0');
+  }
+
+  protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
+    return {
+      pages: [
+        {
+          header: {
+            description: "Configure the webpart"
+          },
+          groups: [
+            {
+              groupName: "Settings",
+              groupFields: [
+                PropertyPaneTextField("clientAppId", {
+                  label: "Client App ID",
+                  description: "The client ID of the app registration created in your Entra ID tenant",
+                  value: this.properties.clientAppId,
+                }),
+                PropertyPaneTextField("functionAppHost", {
+                  label: "Azure function app hostname",
+                  description: "The hostname of your Azure function app",
+                  placeholder: "<yourAppName>.azurewebsites.net",
+                  value: this.properties.functionAppHost,
+                }),
+                PropertyPaneTextField("functionAppCode", {
+                  label: "Azure function app key",
+                  description: "The value of one of the app keys of your Azure function app",
+                  value: this.properties.functionAppCode,
+                }),
+              ]
+            }
+          ]
+        }
+      ],
+    };
+  }
+
+  protected get disableReactivePropertyChanges(): boolean {
+    return false;
   }
 }
