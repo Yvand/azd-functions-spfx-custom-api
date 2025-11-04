119323f689648d3e4463550dea61134ebf07bfc2
Yvand <yvandev@outlook.fr>
2025-11-04 11:00:55 +0100

    update hooks

--- azure-function-app/scripts/remove-app-registration.sh
+++ azure-function-app/scripts/remove-app-registration.sh
@@ -4,6 +4,10 @@
 # -o pipefail: prevents errors in a pipeline from being masked
 set -eo pipefail
 
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
 # https://learn.microsoft.com/en-us/azure/developer/azure-developer-cli/azd-extensibility#use-environment-variables-with-hooks
 # Use the `get-values` azd command to retrieve environment variables from the `.env` file
 while IFS='=' read -r key value; do
@@ -14,16 +18,16 @@ $(azd env get-values)
 EOF
 
 if [ -z "$APP_REGISTRATION_CLIENT_ID" ]; then
-   echo "Could not get the app registration client ID from azd environment file"
+   echo -e "${RED}Could not get the app registration client ID from azd environment file${NC}"
    exit 1
 fi
 
 if [ -z "$AZURE_TENANT_ID" ]; then
-   echo "Could not get the Entra ID tenant ID from azd environment file"
+   echo -e "${RED}Could not get the Entra ID tenant ID from azd environment file${NC}"
    exit 1
 fi
 
-echo "Deleting app registration $APP_REGISTRATION_CLIENT_ID in subscription ${AZURE_SUBSCRIPTION_ID}..."
+echo -e "\nDeleting app registration $APP_REGISTRATION_CLIENT_ID in subscription ${AZURE_SUBSCRIPTION_ID}..."
 az account set --subscription $AZURE_SUBSCRIPTION_ID
 az ad app delete --id $APP_REGISTRATION_CLIENT_ID
-echo "App registration $APP_REGISTRATION_CLIENT_ID deleted successfully."
+echo -e "${YELLOW}App registration $APP_REGISTRATION_CLIENT_ID deleted successfully. Make sure to delete it permanently before provisionning this environment again.${NC}"
--- azure-function-app/scripts/update-solution-package.js
+++ azure-function-app/scripts/update-solution-package.js
@@ -4,8 +4,12 @@ import { readFile, writeFile } from 'fs/promises';
 const packageFilePath = "../spfx-solution/config/package-solution.json"
 const newAppNameValue = process.argv[2];
 
-console.log(`Updating file ${packageFilePath} with new value for the resource: '${newAppNameValue}'`);
+const YELLOW = '\x1b[33m';
+const NC = '\x1b[0m'; // No Color
+
+console.log(`\nUpdating file '${packageFilePath}' with new value for the resource: '${newAppNameValue}'`);
 let fileContent = JSON.parse(await readFile(packageFilePath, "utf8"));
+const oldValue = fileContent.solution.webApiPermissionRequests[0].resource;
 fileContent.solution.webApiPermissionRequests[0].resource = newAppNameValue;
 await writeFile(packageFilePath, JSON.stringify(fileContent, null, 2), { encoding: 'utf8' });
-console.log(`Updated webApiPermissionRequests in ${packageFilePath}:\n${JSON.stringify(fileContent.solution.webApiPermissionRequests, null, 2)}`);
+console.log(`${YELLOW}Updated 'webApiPermissionRequests' in '${packageFilePath}':\nFormer value: ${oldValue}\nNew value: ${newAppNameValue}\nResulting JSON:\n${JSON.stringify(fileContent.solution.webApiPermissionRequests, null, 2)}${NC}`);
